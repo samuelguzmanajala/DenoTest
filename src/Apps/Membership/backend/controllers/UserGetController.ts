19b819bd04458a21aef708ba2c217650f37901e5
samuel <samuelguzmanajala@gmail.com>
2022-08-16 17:10:13 +0200

    [ADD] Finished searchAllController

--- src/Apps/Membership/backend/controllers/UserGetController.ts
+++ src/Apps/Membership/backend/controllers/UserGetController.ts
@@ -1,11 +1,25 @@
 import {Controllers} from "../controllers/Controllers.ts";
-import {Context} from "../../../../dependencies/deps.ts";
 import {User} from "../../../../Contexts/Membership/Users/domain/User.ts";
+import {SearchAllUsersQuery} from "../../../../Contexts/Membership/Users/application/searchAll/SearchAllUsersQuery.ts";
+import {
+    SearchAllUsersQueryHandler
+} from "../../../../Contexts/Membership/Users/application/searchAll/SearchAllUsersQueryHandler.ts";
+import container from "../dependency-injection/Container.ts";
+import {UsersFinder} from "../../../../Contexts/Membership/Users/application/searchAll/UsersFinder.ts";
+import {UsersResponse} from "../../../../Contexts/Membership/Users/application/UsersRespone.ts";
+//import {Context} from "../../../../dependencies/deps.ts";
+
 
 export class UserGetController implements Controllers{
     async run(ctx: any): Promise<void> {
         const params = ctx.params;
         console.log('params', params);
+        const query = new SearchAllUsersQuery();
+        const usersHandler: SearchAllUsersQueryHandler = new SearchAllUsersQueryHandler(container.get(UsersFinder));
+        const userResponse: UsersResponse = await usersHandler.handle(query);
+        ctx.response.headers.set('Access-Control-Allow-Origin', '*');
+        ctx.response.status = 200;
+        ctx.response.body = this.toResponse(userResponse.users);
     }
 
     private toResponse(users: Array<User>) {

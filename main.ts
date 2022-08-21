3697f1a2758612c87c1d075d8d15197c6bb7dfe1
samuel <samuelguzmanajala@gmail.com>
2022-08-21 21:37:00 +0200

    [PRUEBA]

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
 import {Application} from "./src/dependencies/deps.ts";
-
+/*
 const app = new Application();
 app.use((ctx) => {
   ctx.response.body = "Hello World! with oak";
 });
 await app.listen({port: 8000});
+
+ */

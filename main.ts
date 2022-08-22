b073ee07501e535599e0e99446dd94d3621a39b7
samuel <samuelguzmanajala@gmail.com>
2022-08-22 23:27:07 +0200

    [DEPLOY]

--- main.ts
+++ main.ts
@@ -1,7 +1,1 @@
-import {Application, Context} from "oak/mod.ts";
-
-const app = new Application();
-app.use((ctx:Context) => {
-  ctx.response.body = "Hello World! with oak 3 prueba";
-});
-await app.listen({port: 3000});
+console.log('deno cache');

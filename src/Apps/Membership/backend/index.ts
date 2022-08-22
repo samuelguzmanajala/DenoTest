b7d86ef7ec98578d75e773c61bd50e7770c9bc50
samuel <samuelguzmanajala@gmail.com>
2022-08-22 21:22:33 +0200

    [DEPLOY]

--- src/Apps/Membership/backend/index.ts
+++ src/Apps/Membership/backend/index.ts
@@ -1,4 +1,9 @@
-import {UserBackendApp} from "../../../Apps/Membership/backend/UserBackendApp.ts";
+//import {UserBackendApp} from "../../../Apps/Membership/backend/UserBackendApp.ts";
+import {Context, Application, Router} from 'oak/mod.ts';
+import {oakCors} from 'oakCors/mod.ts'
+import Logger from "../../../Contexts/Shared/domain/Logger.ts";
+import container from "./dependency-injection/Container.ts";
+import { registerRoutes } from "./routes/routes.ts";
 
 //import * as nodeProcess from "nodeProcess"
 /*
@@ -17,6 +22,7 @@ function handleError(e: Error) {
 
 */
 
+/*
 
 import {Application, Context} from "oak/mod.ts";
 
@@ -25,3 +31,36 @@ app.use((ctx:Context) => {
   ctx.response.body = "Hello World! from index";
 });
 await app.listen({port: 3000});
+*/
+const port = 3000;
+const logger = container.get(Logger);
+const application = new Application();
+application.use(oakCors());
+const router = new Router();
+registerRoutes(router);
+application.use(router.routes());
+application.use(router.allowedMethods());
+router.use( async (ctx: Context, next)=> {
+    try{
+    await next();
+    logger.success(`${ctx.request.method} ${ctx.request.url.pathname}`);
+    }catch(err){
+    logger.error(err.message);
+    ctx.response.status = err.status || 500;
+    ctx.response.type = 'json';
+    ctx.response.body = {
+        message: err.message
+    };
+    }
+});
+
+
+application.addEventListener("error", (error) => {
+    console.log('error', error);
+    logger.error(error.message);
+  });
+  application.addEventListener("listen", () => {
+    logger.info(`  Backend App is running at http://localhost:${this.port}`);
+    logger.info('  Press CTRL-C to stop\n');
+  });
+  await application.listen({port: Number(port)});

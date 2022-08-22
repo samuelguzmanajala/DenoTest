513f08b18815fab3570590cbb55c2493d5580a78
samuel <samuelguzmanajala@gmail.com>
2022-08-22 23:22:10 +0200

    [DEPLOY]

--- src/Apps/Membership/backend/routes/routes.ts
+++ src/Apps/Membership/backend/routes/routes.ts
@@ -1,10 +1,11 @@
 import {Router} from 'oak/mod.ts';
 import {getFilesList} from "./FilterRoutes.ts";
 
-export async function registerRoutes(router: Router):Promise<void> {
+export function registerRoutes(router: Router):Promise<void> {
     const __dirname = new URL(".", import.meta.url).pathname;
-    const routes = await getFilesList(__dirname, {extension: 'route.ts'});
-    routes.map((route:string) => register(route, router));
+    getFilesList(__dirname, {extension: 'route.ts'}).then((routes)=>{
+        routes.map((route:string) => register(route, router));
+    });
 }
 async function register(routePath: string, router: Router):Promise<void> {
     const route = await import(routePath);

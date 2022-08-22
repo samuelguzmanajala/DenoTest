408a3eb870c7e32226dabf08ede99a9638186474
samuel <samuelguzmanajala@gmail.com>
2022-08-22 07:53:31 +0200

    [PRUEBA]

--- main.ts
+++ main.ts
@@ -1,12 +1,13 @@
+/*
 import {Application} from "./src/dependencies/deps.ts";
 const app = new Application();
 app.use((ctx) => {
   ctx.response.body = "Hello World! with oak 2";
 });
 await app.listen({port: 8000});
+ */
 
 
-/*
 import {serve} from 'https://deno.land/std@0.140.0/http/server.ts';
 
 serve((_req)=>{
@@ -14,4 +15,3 @@ serve((_req)=>{
     headers: {"content-type": "text/plain"}
   });
 });
- */

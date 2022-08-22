29fed055a3a9a7a04732da05ae86589c1a33b168
samuel <samuelguzmanajala@gmail.com>
2022-08-22 15:12:35 +0200

    [REFACTOR] translations

--- src/Contexts/Shared/infrastructure/persistence/mongo/MongoClientFactory.ts
+++ src/Contexts/Shared/infrastructure/persistence/mongo/MongoClientFactory.ts
@@ -1,7 +1,7 @@
 import { Nullable } from "../../../domain/Nullable.ts";
 import {MongoClient} from 'mongo/mod.ts';
 import {Types} from '../../../../../Contexts/Shared/domain/types.ts';
-//import "reflection";
+import "reflection";
 import { Service, Inject } from "di/mod.ts";
 import MongoConfig from "./MongoConfig.ts";
 
